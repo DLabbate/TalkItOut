1dc1eea04b6a7a4237bc4a0c7af1a0f0fe6b95fa
Domenic Labbate <domeniclabbate@gmail.com>
2020-01-25 15:24:50 -0500

    Added message (String) to Mood class.

--- app/src/main/java/com/example/talkitout/Classes/Mood.java
+++ app/src/main/java/com/example/talkitout/Classes/Mood.java
@@ -2,11 +2,13 @@ package com.example.talkitout.Classes;
 
 public class Mood {
     String date;
+    String message;
     int intensity;
 
-    Mood(String date,int intensity)
+    Mood(String date,String message,int intensity)
     {
         this.date = date;
+        this.message = message;
         this.intensity = intensity;
     }
 }
