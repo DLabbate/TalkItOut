39e8eb2b53ba0cf00310422b53531cbc2c328001
Gianluca Lepore <gianluca409@gmail.com>
2020-01-25 14:28:53 -0500

    Added new activities and intent functions

--- app/src/main/java/com/example/talkitout/MainActivity.java
+++ app/src/main/java/com/example/talkitout/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.talkitout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Button loginButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button) findViewById(R.id.loginButton);
+
     }
+
+    private Button.OnClickListener onClickLoginButton = new Button.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            login();
+        }
+    };
+
+    private void login() {
+        Intent intent = new Intent(this, selectInput.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/talkitout/selectInput.java
@@ -0,0 +1,14 @@
+package com.example.talkitout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class selectInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_input);
+    }
+}
