103e7148e8f5fed582714aec08935241c64cba42
Domenic Labbate <domeniclabbate@gmail.com>
2020-01-25 14:33:22 -0500

    Added getters and setters to Person class. Made fields protected.

--- app/src/main/java/com/example/talkitout/Person.java
+++ app/src/main/java/com/example/talkitout/Person.java
@@ -1,8 +1,57 @@
 package com.example.talkitout;
 
 public class Person {
-    String id;
-    String username;
-    String name;
-    String password;
+    protected String id;
+    protected String username;
+    protected String name;
+    protected String password;
+
+    Person(String id,String username,String name,String password)
+    {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
 }
