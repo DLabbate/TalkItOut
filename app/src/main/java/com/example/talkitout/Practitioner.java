c5654cc3aafe4e5675569b56203c76c588ffaced
Domenic Labbate <domeniclabbate@gmail.com>
2020-01-25 15:05:05 -0500

    Added Practitioner class

---
+++ app/src/main/java/com/example/talkitout/Practitioner.java
@@ -0,0 +1,10 @@
+package com.example.talkitout;
+
+public class Practitioner extends Person {
+
+    Practitioner(String id,String username,String name,String password)
+    {
+        super(id,username,name,password);
+    }
+
+}
