213be33e0017c6eef078cbc668bf08d2c91883ed
Domenic Labbate <domeniclabbate@gmail.com>
2020-01-25 22:54:37 -0500

    Added registerActivity checkbox and Intents

--- app/src/main/java/com/example/talkitout/Activities/registerActivity.java
+++ app/src/main/java/com/example/talkitout/Activities/registerActivity.java
@@ -2,8 +2,11 @@ package com.example.talkitout.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.example.talkitout.R;
@@ -11,8 +14,9 @@ import com.example.talkitout.R;
 public class  registerActivity extends AppCompatActivity {
 
     DatabaseHelper myDB;
-    Button btnAdd, btnView;
-    EditText editText;
+    CheckBox usertypeCheckBox;
+    EditText nameEditText,usernameEditText,passwordEditText,practitionerEditText;
+    Button signUpButton;
 
 
     @Override
@@ -23,6 +27,47 @@ public class  registerActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.editText);
         btnAdd = (Button) findViewByID(R.id.btnAdd);
         */
+        setupUI();
+        setUpOnClickListeners();
     }
+
+    private void setupUI()
+    {
+        signUpButton = findViewById(R.id.signupButton);
+        usertypeCheckBox = findViewById(R.id.usercheckBox);
+        nameEditText = findViewById(R.id.fullnameEditText);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        practitionerEditText = findViewById(R.id.practitionerEditText);
+    }
+
+    private void setUpOnClickListeners()
+    {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (usertypeCheckBox.isChecked())
+                {
+                    //TO DO
+                    //ADD NEW PRACTITIONER
+                    Intent gotoDisplay = new Intent(registerActivity.this,displayActivity.class);
+                    startActivity(gotoDisplay);
+                }
+
+                else
+                {
+                    //ADD NEW CLIENT
+                    Intent gotoselectInput = new Intent(registerActivity.this,selectInput.class);
+                    startActivity(gotoselectInput);
+                }
+            }
+        });
+    }
+
+
 
 }
