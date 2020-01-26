a14b5f5da8b0c2fe46a969d6b325c81c4ae7eb8d
Matthew Grande <Matthew.grande@hotmail.com>
2020-01-26 00:15:21 -0500

    Created login functionality, and register verifies if a user exists with the same username.

--- app/src/main/java/com/example/talkitout/Activities/MainActivity.java
+++ app/src/main/java/com/example/talkitout/Activities/MainActivity.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.talkitout.Classes.Client;
+import com.example.talkitout.Classes.Practitioner;
 import com.example.talkitout.R;
 
 import java.util.ArrayList;
@@ -17,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
     protected Button loginButton = null;
     protected Button registerButton = null;
+    protected EditText userEditText;
+    protected EditText passEditText;
+
+    public static DatabaseHelper DBHelper;
+    public static ArrayList<Client> clients;
+    public static ArrayList<Practitioner> practitioners;
+
+    public enum status  {
+        Practitioner,
+        Client
+    };
+
+    public static String loggedInUser = "";
+    public static status loggedInStatus;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,19 +49,25 @@ public class MainActivity extends AppCompatActivity {
         registerButton = (Button) findViewById(R.id.registerButton);
         registerButton.setOnClickListener(onClickRegisterButton);
 
-        //**TEST CODE*************************************************************
-        DatabaseHelper DBhelper = new DatabaseHelper(MainActivity.this);
-        DBhelper.addClientData("dlabbate","Domenic Labbate","mypassword","1234");
-        DBhelper.addClientData("mgrande","MGrande","hispassword","4321");
-        ArrayList<Client> testclients = DBhelper.getAllClients();
-        for (int i=0 ; i<testclients.size(); i++)
-        {
-            String username = testclients.get(i).getUsername();
-            String name = testclients.get(i).getName();
-            String password = testclients.get(i).getPassword();
-            String practitioner = testclients.get(i).getPractitioner();
-            System.out.println("*****USERNAME: "+username + " NAME: " +name+ " PASSWORD: " +password + " PRACTITIONER " +practitioner);
-        }
+        userEditText = findViewById(R.id.userEditText);
+        passEditText = findViewById(R.id.passEditText);
+
+        DBHelper = new DatabaseHelper(MainActivity.this);
+        clients = DBHelper.getAllClients();
+        practitioners = DBHelper.getAllPractitioners();
+
+//        //**TEST CODE*************************************************************
+//        DBHelper.addClientData("dlabbate","Domenic Labbate","mypassword","1234");
+//        DBHelper.addClientData("mgrande","MGrande","hispassword","4321");
+//        ArrayList<Client> testclients = DBhelper.getAllClients();
+//        for (int i=0 ; i<testclients.size(); i++)
+//        {
+//            String username = testclients.get(i).getUsername();
+//            String name = testclients.get(i).getName();
+//            String password = testclients.get(i).getPassword();
+//            String practitioner = testclients.get(i).getPractitioner();
+//            System.out.println("*****USERNAME: "+username + " NAME: " +name+ " PASSWORD: " +password + " PRACTITIONER " +practitioner);
+//        }
 
         //************************************************************************
     }
@@ -55,7 +81,33 @@ public class MainActivity extends AppCompatActivity {
 
     private void login() {
         Intent intent = new Intent(this, selectInput.class);
-        startActivity(intent);
+
+        String usernameInput = userEditText.getText().toString();
+        String passwordInput = passEditText.getText().toString();
+
+        for (int i=0 ; i<clients.size(); i++) {
+            Client current = clients.get(i);
+            if (current.getUsername().equals(usernameInput) &&
+                    current.getPassword().equals(passwordInput)) {
+                this.loggedInUser = usernameInput;
+                this.loggedInStatus = status.Client;
+                startActivity(intent);
+                return;
+            }
+        }
+
+        for (int i=0 ; i<practitioners.size(); i++) {
+            Practitioner current = practitioners.get(i);
+            if (current.getUsername().equals(usernameInput) &&
+                    current.getPassword().equals(passwordInput)) {
+                this.loggedInUser = usernameInput;
+                this.loggedInStatus = status.Practitioner;
+                startActivity(intent);
+                return;
+            }
+        }
+
+        Toast.makeText(this, "Incorrect Username or Password", Toast.LENGTH_SHORT).show();
     }
 
     private Button.OnClickListener onClickRegisterButton = new Button.OnClickListener(){
