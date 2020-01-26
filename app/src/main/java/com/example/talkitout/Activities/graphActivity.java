0e9502770d464cbe688b6a0666f74a6359596e1c
Gianluca Lepore <gianluca409@gmail.com>
2020-01-26 05:15:15 -0500

    created graph activty

---
+++ app/src/main/java/com/example/talkitout/Activities/graphActivity.java
@@ -0,0 +1,35 @@
+package com.example.talkitout.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.talkitout.R;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class graphActivity extends AppCompatActivity {
+
+    LineGraphSeries<DataPoint> series;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+
+        double y,x;
+        x = -5.0;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<DataPoint>();
+        for(int i=0; i<500; i++){
+            x = x + 0.1;
+            y = x;
+            series.appendData(new DataPoint(x,y), true, 500);
+        }
+
+        graph.addSeries(series);
+
+    }
+}
